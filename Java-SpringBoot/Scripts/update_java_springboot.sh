553ecfd1b7019457750ea3eb13fd6e31d4b3ef83
PabloGranado <pablorobertogranadofalcon@alumno.ieselrincon.es>
2025-11-24 09:20:34 +0000

    Update update_java_springboot.sh

--- Java-SpringBoot/Scripts/update_java_springboot.sh
+++ Java-SpringBoot/Scripts/update_java_springboot.sh
@@ -8,16 +8,16 @@ APP_DIR="$APP_HOME/webapp/webapp"
 cd "$APP_DIR"
 
 wget -O src/main/java/com/example/webapp/controller/MainController.java \
-"https://github.com/Nahuel472/imw-vps-apps/raw/main/Java-SpringBoot/controller/MainController.java"
+"https://github.com/Nahuel472/imw-vps-apps/raw/main/Java-SpringBoot/Modificaciones/controller/MainController.java"
 
 wget -O src/main/resources/templates/index.html \
-"https://raw.githubusercontent.com/Nahuel472/imw-vps-apps/main/Java-SpringBoot/index.html"
+"https://raw.githubusercontent.com/Nahuel472/imw-vps-apps/main/Java-SpringBoot/Modificaciones/index.html"
 
 wget -O src/main/resources/templates/contacto.html \
-"https://raw.githubusercontent.com/Nahuel472/imw-vps-apps/main/Java-SpringBoot/contacto.html"
+"https://raw.githubusercontent.com/Nahuel472/imw-vps-apps/main/Java-SpringBoot/Modificaciones/contacto.html"
 
 wget -O src/main/resources/application.properties \
-"https://raw.githubusercontent.com/Nahuel472/imw-vps-apps/main/Java-SpringBoot/application.properties"
+"https://raw.githubusercontent.com/Nahuel472/imw-vps-apps/main/Java-SpringBoot/Modificaciones/application.properties"
 
 sed -i 's/^server\.port=.*/server.port=8081/' src/main/resources/application.properties
 
